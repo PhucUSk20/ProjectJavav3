11286bb318e0781f030dca959c68b01140049999
PhucUSk20 <20200311@student.hcmus.edu.vn>
2023-10-31 09:20:26 +0700

    first commit

---
+++ openCV/build/intermediates/cxx/Debug/3w2l1s86/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\nguye\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Document_C\\Java\\ProjectJavav2-master\\ProjectJavav2-master\\openCV\\.cxx\\Debug\\3w2l1s86\\arm64-v8a" ^
+  opencv_jni_shared
